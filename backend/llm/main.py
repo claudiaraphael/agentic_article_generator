35b83e03d029d3b7e18688b69e2f86df49d147d9
main <claudia.raphael73@gmail.com>
2026-01-16 00:39:03 -0300

    check gpu usage

--- backend/llm/main.py
+++ backend/llm/main.py
@@ -2,25 +2,22 @@ import os
 from dotenv import load_dotenv
 from transformers import pipeline
 
+load_dotenv("HUGGING_FACE_API_KEY")
 
-
-from prompts import research_report_prompt, analysis_report_prompt, linkedin_post_prompt
-from schemas.article import ArticleSchema
-
-
-load_dotenv()
 
 gemini_agent = pipeline(
     "text-generation",
     model="gemini-1.5-flash",
     api_key=os.getenv("GEMINI_API_KEY"),
-    device=0 # Use GPU (0 refers to the first GPU)
+    device=0  # Use GPU (0 refers to the first GPU)
 )
 
 output = gemini_agent("How to get started with langchain?")
 print(output)
 
 # research agent using Gemini API
+
+
 class researcher_agent():
     def __init__(self):
         self.api_key = os.getenv("GEMINI_API_KEY")
